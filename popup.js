c8b4bd72776d4a253046d4160c378a2d1fc3cc6e
Naplyrroth <71039389+Naplyrroth@users.noreply.github.com>
2020-11-09 16:05:12 +0100

    Add files via upload
    Fichiers JS du cours de la semaine JS

---
+++ popup.js
@@ -0,0 +1,4 @@
+window.confirm("BONJOUR MONSIEUR");
+alert("Nous Vous Présentont Notre Site Internet");
+alert("Allez-vous nous donner une bonne note ?");
+nVarNom = prompt("Allez-vous me donner une bonne note ?","oui")
