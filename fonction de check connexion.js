2da24854f599660b6ff181cb0a6abc48b1b629e7
Brian <58400498+nairb725@users.noreply.github.com>
2020-11-12 11:02:17 +0100

    Update fonction de check connexion.js

--- fonction de check connexion.js
+++ fonction de check connexion.js
@@ -1,1 +1,32 @@
-dz
+let mail1 = document.getElementById("mail1");
+let motdepasse1 = document.getElementById("motdepasse1");
+
+let mail2 = document.getElementById("mail2");
+let motdepasse2 = document.getElementById("motdepasse2");
+
+
+
+
+mail1.addEventListener("click",connexion);
+motdepasse1.addEventListener("click",connexion);
+
+mail2.addEventListener("click",connexion);
+motdepasse2.addEventListener("click",connexion);
+
+
+
+
+function connexion() {
+    if {
+
+        mail1.innerHTML = mail2.innerHTML
+        motdepasse1.innerHTML = motdepasse2.innerHTML
+        window.location = "accueil.html"
+
+    } else {
+        window.confirm("ERROR");
+        alert("mot de passe ou mail invalide");
+
+    }
+}
+
