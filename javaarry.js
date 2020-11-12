30a7835ce8a85b915e41b54f117357cc7ae2fecd
Brian <58400498+nairb725@users.noreply.github.com>
2020-11-12 17:33:11 +0100

    Add files via upload
    tableau des fruits a link avec un html (résultat dans la console)

---
+++ javaarry.js
@@ -0,0 +1,9 @@
+let array = ["mangue", "fraise", "pomme", "poire", "kiwi", "citron", "pêche", "mûre", "banane"];
+
+let fruit = "pêche"; //Elément à rechercher
+
+for(let i=0; i<array.length; i++) {
+	if(fruit === array[i]) {
+		console.log('Element Found');
+    } 
+}
