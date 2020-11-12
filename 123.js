68e30288d72be0386aeb40b88470567d5e1a59ab
Brian <58400498+nairb725@users.noreply.github.com>
2020-11-12 14:10:15 +0100

    Update 123.js
    bouton musique

--- 123.js
+++ 123.js
@@ -3,25 +3,39 @@ let titre = document.getElementById("titre");
 let buttongreen = document.getElementById("buttongreen");
 let buttonblue = document.getElementById("buttonblue");
 let buttonred = document.getElementById("buttonred");
+let buttonpurple = document.getElementById("buttonpurple");
 
 
 buttongreen.addEventListener("click",NOSCRUBS);
 buttonred.addEventListener("click",ALTITUDETRAIN);
-buttonblue.addEventListener("click",changetitle);
+buttonblue.addEventListener("click",AFTERDARK);
+buttonpurple.addEventListener("click",PARADISECITY);
+
 
 function NOSCRUBS() {
 
-    titre.innerHTML = "HI"
+    
     window.location.href = "https://www.youtube.com/watch?v=IBdQ4-sCdZw";
 }
 
 
 function ALTITUDETRAIN() {
 
-    titre.innerHTML = "HI"
+   
     window.location.href = "https://www.youtube.com/watch?v=z-vRB294lJQ";
 }
 
+function AFTERDARK() {
+
+    
+    window.location.href = "https://www.youtube.com/watch?v=waAlgFq9Xq8";
+}
+
+function PARADISECITY() {
+
+    
+    window.location.href = "https://www.youtube.com/watch?v=Rbm6GXllBiw";
+}
 
 
 console.log(buttongreen);
